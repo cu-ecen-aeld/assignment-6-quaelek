0ee660f234ca102bb0bfc239dd9ad018af2ce1fa
quaelek <quaelek@Ubuntu.myguest.virtualbox.org>
2024-03-15 13:39:12 -0400

    assignment 6 complete

--- unit-test.sh
+++ unit-test.sh
@@ -5,7 +5,7 @@
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
 
-#mkdir -p build
+mkdir -p build
 #cd build
 #cmake ..
 #make clean
